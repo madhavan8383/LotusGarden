05bdb0ea109305fff43fba08992e945bc760e2cd
madhavan8383 <cool24bar7@gmail.com>
2019-04-15 20:56:18 +0530

    My first commit-4

--- src/com/sample/program/logic/MyFirstProgram.java
+++ src/com/sample/program/logic/MyFirstProgram.java
@@ -6,5 +6,6 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("My first program");
         System.out.println("My first program-2");
+        System.out.println("My first program-4");
     }
 }
